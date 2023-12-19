2ed9faf0bc70d0d406352b7146b0fd8c63d6799d
Fareed-ud-Din-Munawwar <fareeduddin.munawwar@edly.io>
2023-12-19 15:40:46 +0500

    scrollable tabs replaced with TabRows

--- app/src/main/java/com/example/ktorcomposeproject/ui/presentation/component/CategoryTabRow.kt
+++ app/src/main/java/com/example/ktorcomposeproject/ui/presentation/component/CategoryTabRow.kt
@@ -1,15 +1,11 @@
 package com.example.ktorcomposeproject.ui.presentation.component
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.PagerState
-import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,9 +18,8 @@ fun CategoryTabRow(
     categories: List<String>,
     onTabSelected: (Int) -> Unit
 ) {
-    ScrollableTabRow(
+    TabRow(
         selectedTabIndex = pagerState.currentPage,
-        edgePadding = 2.dp,
         containerColor = MaterialTheme.colorScheme.primary,
         contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
     ) {
